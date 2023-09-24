be40b9c5e0f8549f487bfa415b89f4485a26383b
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-09-24 08:48:46 +0600

    added resolver function

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// db
+import db from "./_db.js";
+
 // types
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+
+// resolvers
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
 
 // server setup
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
